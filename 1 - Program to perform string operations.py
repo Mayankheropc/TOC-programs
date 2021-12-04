7bb2727968b6b4edb656c914f044819b81e5e030
Mayank Singh <67374046+Mayankheropc@users.noreply.github.com>
2021-12-04 16:58:34 +0530

    Add files via upload

---
+++ 1 - Program to perform string operations.py
@@ -0,0 +1,39 @@
+# Program to perform string operations
+# By - Mayank Singh
+# Date of Creation - 04-12-2021
+# Last Modified - 04-12-2021
+
+from typing import final
+
+string1 = input("Enter first name: ")
+string2 = input("Enter last name: ")
+
+# combining these 2 strings
+finalstr = string1 + " " + string2
+print("Full name is:", finalstr)
+
+# Total no. of characters including space
+print("Total no. of characters including space:", len(finalstr))
+
+# converting to Upper Case
+print("Full name in UPPERCASE is:", finalstr.upper())
+
+# converting to Lower Case
+print("Full name in lowercase is:", finalstr.lower())
+
+# combining string without space
+finalstr = string1 + string2
+print("Full name without space:", finalstr)
+
+# Total no. of characters without including space
+print("Total no. of characters including space:", len(finalstr))
+
+# Output
+# Enter first name: Mayank
+# Enter last name: Singh
+# Full name is: Mayank Singh
+# Total no. of characters including space: 12
+# Full name in UPPERCASE is: MAYANK SINGH
+# Full name in lowercase is: mayank singh
+# Full name without space: MayankSingh
+# Total no. of characters including space: 11
