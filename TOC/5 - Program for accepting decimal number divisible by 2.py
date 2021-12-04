16d5914b5c6b51e90649fdfbdf72d2c9fa786ec6
Mayank Singh <67374046+Mayankheropc@users.noreply.github.com>
2021-12-04 16:54:44 +0530

    Add files via upload

---
+++ TOC/5 - Program for accepting decimal number divisible by 2.py
@@ -0,0 +1,17 @@
+# program for accepting decimal number divisible by 2
+# By - Mayank Singh
+# Date of Creation - 04-12-2021
+# Last Modified - 04-12-2021
+
+myinput = int(input("Enter a number: "))
+
+if myinput%2 == 0:
+    print("Given number is divisible by 2")
+else:
+    print("Given number is not divisible by 2")
+
+# Output
+# Enter a number: 12
+# Given number is divisible by 2|
+# Enter a number: 111
+# Given number is not divisible by 2
